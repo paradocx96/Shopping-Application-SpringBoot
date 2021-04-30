ccbc0c854197c47d7fe4151097ac8cb60b675f69
Amarangi Karunathilaka <amarangikarunathilaka@gmail.com>
2021-04-30 06:38:39 +0530

    start spring boot application

---
+++ src/test/java/lk/sliit/shoppingapplication/ShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.sliit.shoppingapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
