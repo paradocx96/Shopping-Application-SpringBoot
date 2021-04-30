0a6dfe358c05e478a8dc8e324abb0c1df76e84b9
Amarangi Karunathilaka <amarangikarunathilaka@gmail.com>
2021-04-30 08:45:04 +0530

    create product service and product Repository.

--- src/main/java/lk/sliit/shoppingapplication/ProductRepository.java
+++ src/main/java/lk/sliit/shoppingapplication/ProductRepository.java
@@ -1,4 +1,7 @@
 package lk.sliit.shoppingapplication;
 
-public interface ProductRepository {
+import lk.sliit.shoppingapplication.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, String> {
 }
--- src/main/java/lk/sliit/shoppingapplication/service/ProductService.java
+++ src/main/java/lk/sliit/shoppingapplication/service/ProductService.java
@@ -1,4 +1,31 @@
 package lk.sliit.shoppingapplication.service;
 
+import lk.sliit.shoppingapplication.ProductRepository;
+import lk.sliit.shoppingapplication.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public void add(Product product) {
+        productRepository.save(product);
+    }
+
+    public void delete(String id) {
+        productRepository.deleteById(id);
+    }
+
+    public List<Product> listAll() {
+        return productRepository.findAll();
+    }
+
+    public void update(Product product) {
+        productRepository.save(product);
+    }
 }
